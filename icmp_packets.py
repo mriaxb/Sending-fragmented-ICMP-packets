3be1ba432ca531c9a0ff82891657544a8bfb50cc
root <root@kali>
2023-11-23 11:27:50 -0300

    documentation adustment

--- icmp_packets.py
+++ icmp_packets.py
@@ -1,28 +1,32 @@
+# ---- scapy library import ----
 from scapy.all import *
 
-# Função para criar e enviar pacotes ICMP com fragmentos da carga útil
+# ---- function to create and send ICMP packets in fragments ----
 def send_fragmented_icmp_packets(destination_ip, payload):
-    # Fragmenta a carga útil em pedaços (tamanho ajustável)
+
+# ---- fragmentation of the load into pieces (adjustable size) ----    
     fragment_size = 1
     payload_fragments = [payload[i:i+fragment_size] for i in range(0, len(payload), fragment_size)]
 
-    # Envia cada fragmento como um pacote ICMP separado
+    # ---- each fragment is sent as a separate ICMP packet ----
     for fragment in payload_fragments:
-        # Criação do pacote ICMP Echo Request com o fragmento da carga útil
+
+        # ---- creating ICMP Echo Request packet with payload fragment ----
         icmp_packet = IP(dst=destination_ip) / ICMP() / Raw(load=fragment)
 
-        # Exibição do pacote criado
-        print(f"Pacote ICMP fragmentado:")
+        # ---- showing the ICMP fragmented packet ----
+        print(f"Fragmented ICMP packet:")
         icmp_packet.show()
 
-        # Envio do pacote
+        # ---- package shipping ----
         send(icmp_packet)
 
-# Exemplo de uso
+# ---- example of use ----s
 if __name__ == "__main__":
-    # IP de destino e carga útil personalizada
+
+    # destination IP and custom payload 
     target_ip = "192.168.3.20"
     custom_payload = "SSI{H4ck3rs_w1n}"
 
-    # Chama a função para enviar fragmentos da carga útil em pacotes ICMP
+    # ---- call function to send payload fragments in ICMP packets ----
     send_fragmented_icmp_packets(target_ip, custom_payload)
